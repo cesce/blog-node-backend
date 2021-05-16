09c4a3eb94eac0fc9dfd4b59d8accf8a2657c1a7
cesce <31965498+cesce@users.noreply.github.com>
2021-05-16 22:51:44 +0200

    Updated comments in post and get posts

--- posts/index.js
+++ posts/index.js
@@ -7,22 +7,39 @@ const port = process.env.PORT || 4000;
 // 
 const posts = {};
 
-app.use(express.urlencoded()); //Parse URL-encoded bodies
+app.use(express.urlencoded({
+    extended: true
+})); //Parse URL-encoded bodies
 app.use(express.json());
 
 app.get('/posts', (req, res) => {
+    /*
+    {
+        "a80a08ea": {
+            "id": "a80a08ea",
+            "title": "Post01"
+        }
+    }
+    */
     res.send(posts);
 });
 
-app.post('posts', (req, res) => {
+app.post('/posts', (req, res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
-
+    // req.body => { title: 'Post01' }
+    // title =>  Post01
+    /*
+    {
+        "id": "a80a08ea",
+        "title": "Post01"
+    }
+    */
     posts[id] = {
         id, title
     };
 
-    res.status(201).send(post[id]);
+    res.status(201).send(posts[id]);
 });
 
 app.listen(port, () => {
